6656e64e3dc0989e532340f67f71e22ab6b39868
jackie_aws <jackie@aws_workspace>
2023-04-23 15:50:55 -0700

    exercise 3 complete

--- test/exercise_03_deadlock/test__exercise_03_deadlock.cc
+++ test/exercise_03_deadlock/test__exercise_03_deadlock.cc
@@ -7,24 +7,22 @@
 
 using namespace std;
 
-void increment_counter_ver_1 (size_t & counter, std::mutex & mutex_a, std::mutex & mutex_b, size_t num_times)
+void increment_counter_ver_1 (size_t & counter, std::mutex & mutex_a, size_t num_times)
 {
   for (size_t idx=0; idx<num_times; ++idx) {
     this_thread::sleep_for(1ms); // simluate a complicated calculation
     {
       lock_guard<std::mutex> lg_a(mutex_a);
-      lock_guard<std::mutex> lg_b(mutex_b);
       counter++;
     }
   }
 }
 
-void increment_counter_ver_2 (size_t & counter, std::mutex & mutex_a, std::mutex & mutex_b, size_t num_times)
+void increment_counter_ver_2 (size_t & counter, std::mutex & mutex_a, size_t num_times)
 {
   for (size_t idx=0; idx<num_times; ++idx) {
     this_thread::sleep_for(1ms); // simluate a complicated calculation
     {
-      lock_guard<std::mutex> lg_b(mutex_b);
       lock_guard<std::mutex> lg_a(mutex_a);
       counter++;
     }
@@ -38,8 +36,8 @@ TEST_CASE("deadlock example")
   std::mutex   mutex_x;
   std::mutex   mutex_y;
 
-  std::thread t_1(increment_counter_ver_1, std::ref(counter), std::ref(mutex_x), std::ref(mutex_y), num_times);
-  std::thread t_2(increment_counter_ver_2, std::ref(counter), std::ref(mutex_x), std::ref(mutex_y), num_times);
+  std::thread t_1(increment_counter_ver_1, std::ref(counter), std::ref(mutex_x), num_times);
+  std::thread t_2(increment_counter_ver_2, std::ref(counter), std::ref(mutex_x), num_times);
   t_1.join();
   t_2.join();
 
